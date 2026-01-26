ff51e12e57f79163396dbee2ad4e58b28f4f0676
CodeyWaffle <shibatailyt@gmail.com>
2026-01-26 21:23:56 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'pillow-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Install and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Serve files from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
